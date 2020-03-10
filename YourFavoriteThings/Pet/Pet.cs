ef3e26ce30b01259879a001442ebef385089ea76
Kelsey Creel <kelseycreel@gmail.com>
2020-03-09 19:13:30 -0500

    Add project files.

---
+++ YourFavoriteThings/Pet/Pet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YourFavoriteThings.Pet
+{
+    class Pet
+    {
+        public string Animal { get; set; }
+        public bool IsFriendly { get; set; }
+        public string Name { get; set; }
+        public string Greeting { get; set; }
+
+        public Pet(string name, string greeting)
+        {
+            Name = name;
+            Greeting = greeting;
+        }
+
+        public void Greet()
+        {
+            if (!string.IsNullOrEmpty(Animal) && IsFriendly == true)
+            {
+                Console.WriteLine($"{Name}, the {Animal} says {Greeting}!");
+            } else 
+                Console.WriteLine($"{Name} says {Greeting}!");
+        }
+
+        public void Fetch()
+        {
+            if (IsFriendly == true)
+            {
+                Console.WriteLine($"{Name} is playing fetch!");
+            }
+            else
+                Console.WriteLine($"{Name} is not friendly!");
+        }
+
+    }
+}
---
+++ YourFavoriteThings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using YourFavoriteThings.Food;
+using YourFavoriteThings.Pet;
+using YourFavoriteThings.Game;
+
+namespace YourFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pickle = new Food.Food("pickle", "in my belly");
+            pickle.Eat();
+
+            var popcorn = new Food.Food("popcorn", "in my kitchen")
+            {
+                Age = 2
+            };
+            popcorn.Eat();
+
+            var stormy = new Pet.Pet("Stormy", "woof")
+            {
+                Animal = "dog",
+                IsFriendly = true
+            };
+            stormy.Greet();
+            stormy.Fetch();
+
+            var blue = new Pet.Pet("Blue", "bark bark");
+            blue.Greet();
+            blue.Fetch();
+
+            var monopoly = new Game.Game("Monopoly", "board");
+            monopoly.Classify();
+            monopoly.Win(2);
+
+            Console.ReadKey();
+        }
+    }
+}
